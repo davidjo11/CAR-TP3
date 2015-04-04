4256ce4ada4e3e684ae823adac1f16d5892290ba
thibaud <thibaud@Thibaud-PC>
2015-04-04 13:47:44 +0200

    ajout exception

---
+++ src/site/utils/TransfertException.java
@@ -0,0 +1,14 @@
+package site.utils;
+
+/**
+ * Déclenchée quand il a un problème d'interuption de thread.
+ *
+ */
+public class TransfertException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5102331558573108826L;
+
+}
