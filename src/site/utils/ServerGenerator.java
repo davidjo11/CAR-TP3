1de3e9399e878e992f688f8304d17d445fb05949
Josias David <josias@a10p10.fil.univ-lille1.fr>
2015-04-07 11:57:55 +0200

    shell generator done

--- src/site/utils/ServerGenerator.java
+++ src/site/utils/ServerGenerator.java
@@ -2,6 +2,7 @@ package site.utils;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -14,7 +15,6 @@ public class ServerGenerator {
 	 * 	- les numéros des ports (séparés par des virgules et sans espaces)
 	 * 
 	 */
-	@SuppressWarnings("null")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -53,38 +53,47 @@ public class ServerGenerator {
 			String content = "";
 
 			PrintWriter pw;
-			pw = new PrintWriter("../terminaux.sh");
+			pw = new PrintWriter("../launchServers.sh");
 			int aux = 0;
-			List<Integer> lp = new ArrayList<Integer>();
+			//Liste qui contiendra les serveurs déjà créés (on ne peut pas mettre un serveur comme serveur père d'un autre s'il n'a pas encore été créé.
+			List<Integer> alreadyCreatedServers = new ArrayList<Integer>();
+			Integer[] auxPorts;
 			for(int i = 0;i<n; i++){
 				content += "xterm "
 						+ "-geometry \"50x50+0+"+(40*i)+"\" "
 						+ "-title \"Serveur "+ (i+1) +" (port "+ports[i]+")\" "
-						+ "-e java -classpath ../lib:. site."+ type +".serveur.Serveur "+ ports[i] +" \"Site "+ (i+1) +"\" ";
+						+ "-e \"cd bin;java -classpath ../lib:. site."+ type +".serveur.Serveur "+ ports[i] +" \\\"Site "+ (i+1) +"\\\" ";
 
-				for(int j=0;j<ports.length;j++)
-					if(i != j)
-						lp.add(ports[j]);
+				//Récupération des serveurs déjà créés.
+				auxPorts = new Integer[alreadyCreatedServers.size()];
+				auxPorts = alreadyCreatedServers.toArray(auxPorts);
 
 				if(i != 0)
 					//Pour un graphe, on sépare les ports par les virgules.
 					if(type.equals("graphe")){
 						//On prend un nombre de voisins au hasard.
-						aux = new Random().nextInt(lp.size());
+						aux = new Random().nextInt(alreadyCreatedServers.size());
 						for(int j=0;j<aux;j++){
-							int aux2 = new Random().nextInt(lp.size());
+							int aux2 = new Random().nextInt(alreadyCreatedServers.size());
 							if(j!=0)
-								content += "," + lp.get(aux2);
-							else content += lp.get(aux2);
-							lp.remove(aux2);
+								content += "," + alreadyCreatedServers.get(aux2);
+							else content += alreadyCreatedServers.get(aux2);
+							alreadyCreatedServers.remove(aux2);
 						}
+						//On replace les serveurs qui plausiblement été retirés de la liste 2 lignes plus haut.
+						alreadyCreatedServers = Arrays.asList(auxPorts);
 					}
-					//Pour un arbre, on met un port ou rien.
+				//Pour un arbre, on met un port ou rien.
 					else {
-						aux = new Random().nextInt(lp.size());
-						content += lp.get(aux);
+						aux = new Random().nextInt(alreadyCreatedServers.size());
+						content += alreadyCreatedServers.get(aux);
 					}
-
+				//On ajoute le serveur qui vient d'être créé.
+				alreadyCreatedServers.add(ports[i]);
+				
+				//fin de l'option -e
+				content += "\"";
+				
 				content	+= " &\n";
 
 				content += "sleep 0.05;\n";
